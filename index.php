f8096eaf32c9eae509ebd9a5e47d9cd6e4dd58c5
thatchon <71114727+thatchon@users.noreply.github.com>
2020-10-30 12:09:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <title>ส่งงาน lab itf</title>
+    </head>
+    <body>
+        <h1>ชื่อ-นามสกุล : นายทัชชน เจริญภูวดล</h1>
+        <p>ชื่อเล่น : เบส</p>
+        <img src="mushroom.jpg">
+        <img src="mario.png">
+        <br>
+        <a href="https://onlearn.it.kmitl.ac.th/">This is a Link</a>
+
+    </body>
+</html>
