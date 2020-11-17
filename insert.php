cfb9fe1f58806eb78c2d52fa9f1085d1e20b2bd5
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-18 04:21:13 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'thatchon.mysql.database.azure.com', 'btkeiei1@thatchon', 'THEMARI01z', 'itflab', 3306);
+mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
@@ -17,7 +17,11 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
+  echo "<script type='text/javascript'>";
     echo "New record created successfully";
+    echo "window.location = 'show.php'; ";
+  echo "</script>";
+
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
