72270e69b77208b9b29012af46deb1714d1a588e
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-18 04:28:08 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,14 +17,13 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-  //echo "<script type='text/javascript'>";
-    echo "New record created successfully";
-   // echo "window.location = 'show.php'; ";
- // echo "</script>";
+  echo "<script type='text/javascript'>";
+    //echo "New record created successfully";
+    echo "window.location = 'show.php'; ";
+  echo "</script>";
 
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
   
 mysqli_close($conn);
-?>
