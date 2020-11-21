22aa0a3b6e3515b42921d6ecbb7d8ac8fcc5ea4d
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 20:11:27 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,7 +2,8 @@
 
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'thatchon.mysql.database.azure.com', 'btkeiei1@thatchon', 'THEMARI01z', 'itflab', 3306);
-if (mysqli_connect_errno($conn)) {
+if (mysqli_connect_errno($conn)) 
+{
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
