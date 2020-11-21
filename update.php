4057a4b0c0db5418f892dc6e14d919fc3df8c11e
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 19:22:14 +0700

    Create update.php

---
+++ update.php
@@ -0,0 +1,15 @@
+<?php
+$id=$_GET['ID'];
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'thatchon.mysql.database.azure.com', 'btkeiei1@thatchon', 'THEMARI01z', 'itflab', 3306);
+$name=$_POST['Name'];
+$comment=$_POST['Comment'];
+$link=$_POST['Link'];
+$sql="UPDATE guestbook SET Name='$name',Comment='$comment',Link='$link' WHERE ID='$id'";
+if (mysqli_query($conn, $sql)) {
+    header("Location: show.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+mysqli_close($conn);
+?>
