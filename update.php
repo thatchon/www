cb78ddc825dadae43f266ff0cbedf37f2d2dade7
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 20:22:33 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,8 +2,7 @@
 
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'thatchon.mysql.database.azure.com', 'btkeiei1@thatchon', 'THEMARI01z', 'itflab', 3306);
-if (mysqli_connect_errno($conn)) 
-{
+if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
@@ -13,14 +12,7 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "UPDATE guestbook SET 
-
-Name='$name',
-Comment='$comment',
-Link='$link'
-
-WHERE ID='$id'
-";
+$sql = "UPDATE guestbook SET Name='$name', Comment='$comment', Link='$link' WHERE ID='$id'";
 
 
 if (mysqli_query($conn, $sql)) {
