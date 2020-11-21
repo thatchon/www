032bf580d49fecfa2fd03c87d9ff47c92c794335
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 20:27:02 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,11 @@
 <?php
-
+$id = $_GET['ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'thatchon.mysql.database.azure.com', 'btkeiei1@thatchon', 'THEMARI01z', 'itflab', 3306);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-$id = $_GET['ID'];
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
