bd24797a247e15be48afb7c4defe93ef5fc86cbd
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 21:38:55 +0700

    Update modify.php

--- modify.php
+++ modify.php
@@ -29,17 +29,16 @@
   ?>
 
   <div class="container">
-    <h1>ฟอร์มแก้ไข/ปรับปรุงข้อมูล</h1>
-    <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
-        <label for="Username">Username :</label>
-          <input type="text" class="form-control" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
-          <label for="Username">Comment :</label>
-          <input type="text" class="form-control" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
-        <label for="Username">Link :</label>
-          <input type="text" class="form-control" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
-          <input type="submit" id="commentBtn"class="btn btn-outline-warning">
+    <h2>Edit Form</h2>
+    <form action="update.php?ID=<?php echo $row['ID']; ?>" method="post" id="CommentForm">
+      <label for="Username">Username :</label>
+      <input type="text" class="form-control" name="Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
+      <label for="Username">Comment :</label>
+      <input type="text" class="form-control" name="Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
+      <label for="Username">Link :</label>
+      <input type="text" class="form-control" name="Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
+      <input type="submit" id="commentBtn" class="btn btn-outline-warning">
   </div>
-</form>
-</body>
+  </form>
 
 </html>
