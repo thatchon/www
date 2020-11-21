e1b4976abf05c2601e5aa2c11b775e0ad0d7d7f3
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 23:03:38 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -8,6 +8,14 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
 </head>
     <body>
     <?php
@@ -20,10 +28,13 @@
     <div class="container">
     <h2>Edit Form</h2>
     <form action = "update.php?ID=<?php echo $row['ID']; ?>" method = "post" id="CommentForm">
+        <i class="fas fa-user input-prefix"></i>
         <label for="Username">Username :</label>
           <input type="text" class="form-control" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
-          <label for="Username">Comment :</label>
+        <i class="fas fa-user input-prefix"></i>
+        <label for="Username">Comment :</label>
           <input type="text" class="form-control" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
+        <i class="fas fa-user input-prefix"></i>
         <label for="Username">Link :</label>
           <input type="text" class="form-control" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
           <input type="submit" id="commentBtn"class="btn btn-outline-warning">
