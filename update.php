27f89e4afcfd53a42ddc998db18f265d4daaa231
thatchon <71114727+thatchon@users.noreply.github.com>
2020-11-21 20:03:17 +0700

    Update update.php

--- update.php
+++ update.php
@@ -2,28 +2,32 @@
 
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'thatchon.mysql.database.azure.com', 'btkeiei1@thatchon', 'THEMARI01z', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-
 
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "UPDATE guestbook SET Name='$Name',Comment='$Comment',Link='$Link' WHERE ID='$ID'";
+$sql = "UPDATE guestbook SET 
+
+Name='$name',
+Comment='$comment',
+Link='$link'
+
+WHERE ID='$id'
+";
 
 
 if (mysqli_query($conn, $sql)) {
-  echo "<script type='text/javascript'>";
+    echo "<script type='text/javascript'>";
     //echo "New record created successfully";
     echo "window.location = 'show.php'; ";
-  echo "</script>";
-
-  } else {
+    echo "</script>";
+} else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
+}
+
 mysqli_close($conn);
